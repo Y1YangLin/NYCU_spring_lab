27e184fb520b544fae99dba1b514412276454ae8
LYY <minecr225@gmail.com>
2025-02-02 22:56:23 +0800

    doc: adding TODOs

--- Single_Job_players.py
+++ Single_Job_players.py
@@ -1,4 +1,14 @@
 class Single_Job_players:
     
-    def __init__(self):
+    
+    def __init__(self, numOfPlayers: int, P: list, W: list):
+        self.numOfPlayers = numOfPlayers
+        self.P = P
+        self.W = W
+    
+    def caculateRhoValues(self):
         pass
+# TODO
+# 1. 將 Players, P, W的數量輸入到建構子中
+# 2. 算出 Rho Values，並存在class 中
+
