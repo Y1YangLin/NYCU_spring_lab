39358e89e6486e5d23589c743d1df00d8efd6434
LYY <minecr225@gmail.com>
2025-02-08 01:28:19 +0800

    feat: 建立好 trie 並且測試 search function 可以查找到 job i 在 job j 前的所有可能性

--- Single_Job_players.py
+++ Single_Job_players.py
@@ -1,14 +1,56 @@
+import numpy as np
+import random
+import itertools
+from Trie import Trie
 class Single_Job_players:
     
-    
-    def __init__(self, numOfPlayers: int, P: list, W: list):
+    def __init__(self, numOfPlayers:int, P:list, W:list):
         self.numOfPlayers = numOfPlayers
         self.P = P
         self.W = W
-    
-    def caculateRhoValues(self):
-        pass
-# TODO
-# 1. 將 Players, P, W的數量輸入到建構子中
-# 2. 算出 Rho Values，並存在class 中
 
+    def calculateRhoValues(self):
+        return [round(self.P[i] / self.W[i], 3) for i in range(self.numOfPlayers)]
+    
+    def jobPermutations(self):
+        permutations = list(itertools.permutations(range(self.numOfPlayers))) 
+        perm_dict = { i : tuple(j + 1 for j in perm) for i, perm in enumerate(permutations)}
+        letter_dict = {i: chr(97 + i) for i in range(len(permutations))} #不一定要
+        return perm_dict, letter_dict
+    
+    # 計算Rho prime values
+    # return rpvalues : list[]
+    def calculateRpValues(self):
+        pass
+
+
+# Main code
+players = int(input("Please enter the number of players :")) 
+p = []
+w = []
+
+for i in range(players) :
+    p.append(random.randint(1, 10))
+    w.append(random.randint(1, 10))
+
+data = Single_Job_players(players, p, w)
+permutations_dict, letter_dict = data.jobPermutations()
+rho = data.calculateRhoValues()
+
+
+print(data.numOfPlayers)
+print(data.P)
+print(data.W)
+print(rho)
+print("Job Permutations : ")
+for key, value in permutations_dict.items():
+    print(f"{key} : {value} -> {letter_dict[key]}")
+
+    
+trie = Trie()
+for value in permutations_dict.values():
+    str_value = "" . join(map(str, value))
+    trie.insert(str_value)
+    
+# 測試是否可以依照設定 print 出 Job i 在 Job j 前的可能排列
+print(trie.search_valid_permutations("3", "1"))
--- Trie.py
+++ Trie.py
@@ -1,3 +1,31 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
 class Trie:
     def __init__(self):
-        pass
+        self.root = TrieNode()
+    
+    def insert(self, word: str):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+        
+    def search_valid_permutations(self, fromIdx: str, toIdx: str, node=None, path="", results=None) -> list:
+        if results is None:
+            results = []
+        if node is None:
+            node = self.root
+            
+        if node.is_end:
+            if fromIdx in path and toIdx in path and path.index(fromIdx) < path.index(toIdx):
+                results.append(path)
+        # DFS search
+        for char in sorted(node.children.keys()):
+            self.search_valid_permutations(fromIdx, toIdx, node.children[char], path + char,results)
+        return results
+                
